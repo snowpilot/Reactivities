58b89d6b3098b71beb2b3a8284238c787e1b10c2
Wolfgang Fellenz <wolfgang@fellenz.de>
2025-01-10 18:01:47 +0100

    End of section 15

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
-import { Activity } from '../models/activity'
+import { IActivity, ActivityFormValues } from '../models/activity'
 import { toast } from 'react-toastify';
 import { router } from '../router/Routes';
 import { store } from '../stores/store';
@@ -66,11 +66,12 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities'),
-    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => axios.post<void>('/activities', activity),
-    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id: string) => axios.delete<void>(`/activities/${id}`)
+    list: () => requests.get<IActivity[]>('/activities'),
+    details: (id: string) => requests.get<IActivity>(`/activities/${id}`),
+    create: (activity: ActivityFormValues) => requests.post<void>('/activities', activity),
+    update: (activity: ActivityFormValues) => requests.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del<void>(`/activities/${id}`),
+    attend: (id: string) => requests.post<void>(`/activities/${id}/attend`, {})
 }
 
 const Account = {
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { useNavigate, useParams } from "react-router-dom";
-import { Activity } from "../../../app/models/activity";
+import { Activity, ActivityFormValues } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponents";
 import {v4 as uuid} from 'uuid';
 import { Formik, Form } from "formik";
@@ -17,19 +17,11 @@ import MyDateInput from "../../../app/common/form/MyDateInput";
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
     const { createActivity, updateActivity, 
-        loading, loadActivity, loadingInitial } = activityStore;
+        loadActivity, loadingInitial } = activityStore;
     const {id} = useParams();
     const navigate = useNavigate();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -41,15 +33,31 @@ export default observer(function ActivityForm() {
     })
 
     useEffect(() => {
-        if (id) loadActivity(id).then(activity => setActivity(activity!))
+        if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)))
     }, [id, loadActivity]);
 
-    function handleFormSubmit(activity: Activity) {
+    function handleFormSubmit(activity: ActivityFormValues) {
         if (!activity.id) {
+            const newActivity: Activity = {            
+                ...activity,
+                id: uuid(),
+                hostUsername: '',
+                isCancelled: false,
+                isGoing: false,
+                isHost: false
+            }
             activity.id = uuid();
-            createActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+            createActivity(newActivity).then(() => navigate(`/activities/${newActivity.id}`))
         } else {
-            updateActivity(activity).then(() => navigate(`/activities/${activity.id}`))
+            const updatedActivity: Activity = {
+                ...activity,
+                id: activity.id || uuid(),
+                hostUsername: '',
+                isCancelled: false,
+                isGoing: false,
+                isHost: false
+            };
+            updateActivity(updatedActivity).then(() => navigate(`/activities/${activity.id}`))
         }
     }
 
@@ -80,7 +88,7 @@ export default observer(function ActivityForm() {
                         <MyTextInput placeholder='Venue' name='venue' />
                         <Button 
                             disabled={isSubmitting || !dirty || !isValid }
-                            loading={loading} floated="right" 
+                            loading={isSubmitting} floated="right"
                             positive type='submit' content='Submit' />
                         <Button floated="right" type='button' content='Cancel' />
                     </Form>                                       
